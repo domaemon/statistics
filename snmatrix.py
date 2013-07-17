ab48561a4d3841aaa1a9566dc8a09f2011e98382
domaemon <makoto.sugano@gmail.com>
2013-07-17 21:55:30 +0300

    the network is now constructed in recursive manner. all the signed-off-by people get connected in the same commit.

--- snmatrix.py
+++ snmatrix.py
@@ -12,6 +12,34 @@ from datetime import date, datetime
 #(output, err) = p.communicate()
 #versions = output.splitlines()
 
+def calc_days(prev_version, version):
+    command = 'git log --pretty=format:"%ad" --no-walk ' + version
+
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    (output, err) = p.communicate()
+    m = re.match('\w+ (\w+ \d+) \d+:\d+:\d+ (\d+) .\d+', output)
+    if (m):
+        date_string = m.group(1) + " " + m.group(2)
+        date_obj = datetime.strptime(date_string, '%b %d %Y').date()
+        end_date_obj = date_obj
+    else:
+        exit
+
+    command = 'git log --pretty=format:"%ad" --no-walk ' + prev_version
+
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    (output, err) = p.communicate()
+    m = re.match('\w+ (\w+ \d+) \d+:\d+:\d+ (\d+) .\d+', output)
+    if (m):
+        date_string = m.group(1) + " " + m.group(2)
+        date_obj = datetime.strptime(date_string, '%b %d %Y').date()
+        start_date_obj = date_obj
+    else:
+        exit
+
+    print (end_date_obj - start_date_obj).days
+
+
 def gen_commit_objs_by_version(prev_version, version):
     commit_objs = []
 
@@ -64,10 +92,6 @@ def gen_commit_objs_by_version(prev_version, version):
     return commit_objs
 
 def gen_name_list(commit_objs):
-
-    return name_list
-    
-def gen_network_matrix(commit_objs):
     # generating name_list
     name_list_orig = []
     name_list = []
@@ -78,6 +102,10 @@ def gen_network_matrix(commit_objs):
             name_list_orig.append(name)
 
     name_list = sorted(set(name_list_orig))
+
+    return name_list
+    
+def gen_network_matrix(commit_objs, name_list):
 
     # init network_matrix
     network_matrix = {}
@@ -90,30 +118,58 @@ def gen_network_matrix(commit_objs):
     # compute network_matrix
     for commit_obj in commit_objs:
         network_size = len(commit_obj['network'])
-        if (network_size > 1):
-            for cnt in (range(network_size - 1)):
-                from_name = commit_obj['network'][cnt]
-                to_name   = commit_obj['network'][cnt + 1]
-                print from_name, to_name
+
+        # the last element can *NOT* be from_name
+        for from_cnt in (range(network_size - 1)):
+            from_name = commit_obj['network'][from_cnt]
+
+            # the last element can be from_name
+            for to_cnt in (range(from_cnt + 1, network_size)):
+                to_name = commit_obj['network'][to_cnt]
                 network_matrix[from_name][to_name] += 1
+        
 
     return network_matrix
+
+def calc_network_density(network_matrix, name_list):
+    lines_cnt = 0
+
+    for from_name in name_list:
+        for to_name in name_list:
+            if network_matrix[from_name][to_name] != 0:
+                lines_cnt += 1
+
+    len_name_list = len(name_list)
+    density = float(lines_cnt) / float((len_name_list) * (len_name_list - 1))
+    print density
+    return density
+
 
 ######
 
 os.chdir(os.getenv("HOME") + '/src/linux')
 
-prev_version="v3.9-rc8"
-versions = ["v3.9"]
+prev_version="v3.6"
+versions = ["v3.7", "v3.8", "v3.9"]
 commit_objs_by_version = {}
 network_matrix_by_version = {}
+days_by_version = {}
 
 ### commit_objs_by_version is generated ###
 for version in versions:
+
     commit_objs = gen_commit_objs_by_version(prev_version, version)
     commit_objs_by_version[version] = commit_objs
-    network_matrix_by_version[version] = gen_network_matrix(commit_objs)
-    print network_matrix_by_version[version]
+
+    days = calc_days(prev_version, version)
+    days_by_version[version] = days
+
+    name_list = gen_name_list(commit_objs)
+
+    network_matrix = gen_network_matrix(commit_objs, name_list)
+    network_matrix_by_version[version] = network_matrix
+
+    density = calc_network_density(network_matrix, name_list)
 
     prev_version = version
 
